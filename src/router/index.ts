da18b6a069de3cbd99f213203fff90b08a206999
Rafikov Timur <rafegg21@gmail.com>
2025-08-28 21:40:22 +0500

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory  } from 'vue-router'
 import { useAuth } from '@/stores/auth'
 
 import LoginPage from '@/pages/LoginPage.vue'
